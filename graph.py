1140179748eaa9bee4eb1fd1cd738c85d64e8709
bishoyKaleny <118350355+bishoyKaleny@users.noreply.github.com>
2023-08-04 15:59:31 +0200

    predciction methods
    addded a method to show the predictions for childs of a node

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 class TreeNode:
     def __init__(self, data,prediction,is_goal=False): #need to edit the prediction so it can be automaticaly the right prediction
         self.data = data
+        self.prediction=prediction
         self.children = []
 
     def add_child(self, child):
@@ -16,28 +17,35 @@ class TreeNode:
             raise ValueError("Child not found")
 
     def __repr__(self, level=0):
-        ret = "\t" * level + repr(self.data) + "\n"
+        ret = "\t" * level + repr(self.data)+repr(self.prediction)+ "\n"
         for child in self.children:
             ret += child.__repr__(level + 1)
         return ret
 
     def __str__(self, level=0):
-        ret = "\t" * level + str(self.data) + "\n"
+        #ret=f"\t*{level} {str(self.data)} prediction: {str(self.prediction)} \n"
+        ret = "\t" * level + str(self.data) +"\n"
         for child in self.children:
             ret += child.__str__(level + 1)
         return ret
-
-
+    
+    def get_prediction_childeren(self):
+        node_prediction=self.prediction
+        hash_predictions={f'{self.data}':self.prediction}
+        for child in self.children:
+            hash_predictions[child.data]=child.prediction
+        return hash_predictions
+            
 # Example usage:
 if __name__ == "__main__":
     # Create the tree structure
-    root = TreeNode("Root")
-    child1 = TreeNode("Child 1")
-    child2 = TreeNode("Child 2")
-    child3 = TreeNode("Child 3")
-    child1_1 = TreeNode("Child 1.1")
-    child1_2 = TreeNode("Child 1.2")
-    child2_1 = TreeNode("Child 2.1")
+    root = TreeNode("Root",6)
+    child1 = TreeNode("Child 1",7)
+    child2 = TreeNode("Child 2",2)
+    child3 = TreeNode("Child 3",4)
+    child1_1 = TreeNode("Child 1.1",5)
+    child1_2 = TreeNode("Child 1.2",3)
+    child2_1 = TreeNode("Child 2.1",2)
 
     root.add_child(child1)
     root.add_child(child2)
@@ -51,6 +59,7 @@ if __name__ == "__main__":
     # Print the tree
     print("Tree structure:")
     print(root)
+    print(child1.get_prediction_childeren())
 
     
         
